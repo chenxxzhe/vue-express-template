071ccf8187a1627e2819a414383de0414bc37fd3
chenxxzhe <chenxxzhe@163.com>
2018-03-30 01:43:57 +0800

    feat: 增加pm2 配置

---
+++ pm2.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+
+  /*
+    安装: cnpm i -g pm2
+    运行: pm2 start pm2.config.js
+  */
+
+  apps: [
+
+    {
+      name: 'dev', // 写你项目名
+      script: 'bin/www',
+      watch: ['config', 'routes'],
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  // deploy: {
+  //   production: {
+  //     user: 'node',
+  //     host: '212.83.163.1',
+  //     ref: 'origin/master',
+  //     repo: 'git@github.com:repo.git',
+  //     path: '/var/www/production',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+  //   },
+  //   dev: {
+  //     user: 'node',
+  //     host: '212.83.163.1',
+  //     ref: 'origin/master',
+  //     repo: 'git@github.com:repo.git',
+  //     path: '/var/www/development',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+  //     env: {
+  //       NODE_ENV: 'dev',
+  //     },
+  //   },
+  // },
+}
